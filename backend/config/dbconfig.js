da3c990e1356e4c1c55500881603ddcc02251524
GARGI GUSAIN <157007752+gargigusain@users.noreply.github.com>
2025-03-04 14:53:15 +0530

    Update dbconfig.js

--- backend/config/dbconfig.js
+++ backend/config/dbconfig.js
@@ -16,9 +16,10 @@ db.connect((err) => {
 });
 
 module.exports = {
-    HOST: process.env.DB_HOST,
-    USER: process.env.DB_USER,
-    PASSWORD: process.env.DB_PASSWORD,
-    DB: process.env.DB_NAME,
-  };
-  
+        HOST: "localhost",
+        USER: "root",
+        PASSWORD: "binary_operation23",
+        DB: "employee_management",
+        dialect: "mysql", // or "postgres" if using PostgreSQL
+};
+     
